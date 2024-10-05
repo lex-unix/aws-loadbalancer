376d54e1248a5499f96f53e3bd17c58d8d21554e
Lex Miin <alexmiyin@gmail.com>
2024-10-05 14:05:49 +0300

    refactor: use base ubuntu for bastion instance

--- ec2.tf
+++ ec2.tf
@@ -18,6 +18,22 @@ data "aws_ami" "ubuntu_nginx" {
   }
 }
 
+data "aws_ami" "ubuntu_base" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
 resource "aws_instance" "web" {
   subnet_id              = aws_subnet.private1.id
   vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_ssh.id]
@@ -40,9 +56,8 @@ resource "aws_instance" "bastion" {
   subnet_id                   = aws_subnet.public1.id
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
   associate_public_ip_address = true
-  ami                         = data.aws_ami.ubuntu_nginx.id
+  ami                         = data.aws_ami.ubuntu_base.id
   instance_type               = "t2.micro"
-  user_data                   = file("init.yaml")
   key_name                    = aws_key_pair.deployer.key_name
   root_block_device {
     volume_type           = "gp2"
